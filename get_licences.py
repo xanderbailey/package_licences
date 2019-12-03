130cbd53fc068b4ecd0ec35b08377b30e60d60d0
zander181 <xander@Alexanders-MacBook-Pro-6.local>
2019-12-03 22:02:34 +0000

    used black

--- get_licences.py
+++ get_licences.py
@@ -3,6 +3,7 @@ import requests
 
 from bs4 import BeautifulSoup
 
+
 def _get_licence(soup):
     """
     Finds the licence for the package from pypi
@@ -18,7 +19,7 @@ def _get_licence(soup):
         licence of package
     """
 
-    sidebars = soup.find_all("h3","sidebar-section__title")
+    sidebars = soup.find_all("h3", "sidebar-section__title")
     for sidebar in sidebars:
         if sidebar.text == "Meta":
             licence = sidebar.find_next("p").text.split("License: ")[-1]
@@ -26,6 +27,7 @@ def _get_licence(soup):
         else:
             licence = "Can't find"
     return licence
+
 
 def _get_github(soup):
     """
@@ -43,7 +45,7 @@ def _get_github(soup):
 
     """
 
-    sidebars = soup.find_all("h3","sidebar-section__title")
+    sidebars = soup.find_all("h3", "sidebar-section__title")
     for sidebar in sidebars:
         if sidebar.text == "Statistics":
             github_api = sidebar.find_next("div").get("data-url")
@@ -52,11 +54,14 @@ def _get_github(soup):
             else:
 
                 github_split = github_api.split("/")[-2:]
-                github = "https://www.github.com/{}/{}".format(github_split[0],github_split[1])
+                github = "https://www.github.com/{}/{}".format(
+                    github_split[0], github_split[1]
+                )
             break
     return github
 
-def get_csv(requirement_dir,dest_dir):
+
+def get_csv(requirement_dir, dest_dir):
     """
     Gets a csv of licences and github repos for each package in requirements txt
 
@@ -74,19 +79,19 @@ def get_csv(requirement_dir,dest_dir):
     """
     root_url = "https://pypi.org/project/"
     rows = []
-    with open(requirement_dir, 'r') as file:
+    with open(requirement_dir, "r") as file:
         lines = [f.split("\n")[0] for f in file]
     packages = [line for line in lines if len(line.split("#")) == 1 and line != ""]
-    
+
     for package in packages:
-        package_list = package.split('==')
+        package_list = package.split("==")
 
         if len(package_list) == 2:
             package_name = package_list[0]
             package_version = package_list[1]
-            
+
         elif len(package_list) == 1:
-            package_list = package.split('>=')
+            package_list = package.split(">=")
             if len(package_list) == 2:
                 package_name = package_list[0]
                 package_version = package_list[1]
@@ -95,26 +100,26 @@ def get_csv(requirement_dir,dest_dir):
                 package_version = "Unkown"
         else:
             print("package error")
-        
+
         url = root_url + package_name
-        
+
         page = requests.get(url)
-        
-        soup = BeautifulSoup(page.content, 'html.parser')
-        
+
+        soup = BeautifulSoup(page.content, "html.parser")
+
         if soup.title.text.find("404") != -1:
             print("{} not found on pypi".format(package_name))
             licence = "Unknown"
             github = "Unknown"
-        else:    
+        else:
             licence = _get_licence(soup)
             github = _get_github(soup)
-        
-        row = [package_name,package_version,licence,url,github]
+
+        row = [package_name, package_version, licence, url, github]
         rows.append(row)
-    
+
     df = pd.DataFrame(rows)
-    df.columns = ["Package","Version","Licence","URL","Gitbub"]
+    df.columns = ["Package", "Version", "Licence", "URL", "Gitbub"]
     df.to_csv(dest_dir)
-    
+
     return df
